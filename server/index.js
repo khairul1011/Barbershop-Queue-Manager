372ab2b933ee8fd9b81c2f4e93f28c6100e2cfd9
Khairul <khairul@MacBook-Air-Khairul.local>
2026-07-26 12:59:26 +0700

    Add backend server/ folder: WhatsApp bot boilerplate + env template

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const { Client, LocalAuth } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
+
+const client = new Client({
+  authStrategy: new LocalAuth(),
+  puppeteer: { args: ['--no-sandbox', '--disable-setuid-sandbox'] }
+});
+
+client.on('qr', qr => qrcode.generate(qr, { small: true }));
+client.on('ready', () => console.log('Client is ready!'));
+client.on('message', msg => console.log(`${msg.from}: ${msg.body}`));
+
+client.initialize();
