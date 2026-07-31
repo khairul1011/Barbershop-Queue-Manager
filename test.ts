1f7387e0abc50d9f56c5a7df2b08ef2aceb2fe81
Khairul <khairul@MacBook-Air-Khairul.local>
2026-08-01 01:05:08 +0700

    chore: remove unused test files and unused express/dotenv dependencies from root

--- test.ts
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-import { cva, type VariantProps } from "class-variance-authority"
-const badgeVariants = cva("test", { variants: { variant: { default: "test" } } })
-export type BadgeProps = React.HTMLAttributes<HTMLDivElement> & VariantProps<typeof badgeVariants>
-const b: BadgeProps = { className: "" };
